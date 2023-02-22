ec025bfe16374b2f0657dabb11fa0b7ccc884b84
Gyorgy Orban <gyorban@cloudera.com>
2023-02-22 09:42:04 +0100

    DEX-9088: reduce the number of warnings in the logs (#43)
    By setting a fallback value the number of warnings
    regarding a missing config value should be reduced.
    
    Added an additional tests to check when a
    wrong configuration value is set. In this
    case the default values should be used.

--- cloudera_airflow_provider/cloudera/airflow/providers/hooks/cde.py
+++ cloudera_airflow_provider/cloudera/airflow/providers/hooks/cde.py
@@ -131,9 +131,13 @@ class CdeHook(BaseHook):  # type: ignore
             num_retries_val = num_retries
         else:
             try:
-                num_retries_val = conf.getint('cde', 'DEFAULT_NUM_RETRIES')
+                num_retries_val = conf.getint(
+                    'cde', 'DEFAULT_NUM_RETRIES', fallback=CdeHook.DEFAULT_NUM_RETRIES
+                )
             except AirflowConfigException as acerr:
                 self.log.warning(acerr)
+                self.log.warning("Falling back to default num retries value: %s",
+                                 CdeHook.DEFAULT_NUM_RETRIES)
 
         self.num_retries = num_retries_val
 
@@ -142,9 +146,13 @@ class CdeHook(BaseHook):  # type: ignore
             api_timeout_val = api_timeout
         else:
             try:
-                api_timeout_val = conf.getint('cde', 'DEFAULT_API_TIMEOUT')
+                api_timeout_val = conf.getint(
+                    'cde', 'DEFAULT_API_TIMEOUT', fallback=CdeHook.DEFAULT_API_TIMEOUT
+                )
             except AirflowConfigException as acerr:
                 self.log.warning(acerr)
+                self.log.warning("Falling back to default api timeout value: %s",
+                                 CdeHook.DEFAULT_API_TIMEOUT)
 
         self.api_timeout = api_timeout_val
 
